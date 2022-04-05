9a40bea1936c56f13c48b97d3bb43ba2ce1a8589
miftah hijriyyah <mhijriyyah@gmail.com>
2022-04-05 13:47:32 +0700

    aplikasi ini menggunakan native php

---
+++ views/beranda.php
@@ -0,0 +1,7 @@
+ <div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-2 text-gray-800"align="center">SELAMAT DATANG</h1>
+<p class="mb-4"><marquee>Halaman ini menampilkan data buku yang ada di Perpustakaan SMK NU.</marquee></p>
+
+</div>
